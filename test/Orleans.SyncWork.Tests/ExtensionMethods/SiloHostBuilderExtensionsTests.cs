4ad744e573945f69a704e04c0079c88b1ebd49d4
Ryan Henderson <ryan.henderson@slalom.com>
2022-12-08 22:30:32 -0600

    Fixed tests

--- test/Orleans.SyncWork.Tests/ExtensionMethods/SiloHostBuilderExtensionsTests.cs
+++ test/Orleans.SyncWork.Tests/ExtensionMethods/SiloHostBuilderExtensionsTests.cs
@@ -1,48 +1,45 @@
-﻿using System;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.Extensions.Configuration;
+﻿using FluentAssertions;
 using Microsoft.Extensions.Hosting;
 using Orleans.Hosting;
-using Orleans.SyncWork.Demo.Services.Grains;
 using Orleans.SyncWork.ExtensionMethods;
-using Orleans.SyncWork.Tests.TestGrains;
-using Orleans.TestingHost;
 using Xunit;
 
 namespace Orleans.SyncWork.Tests.ExtensionMethods;
 
 public class SiloHostBuilderExtensionsTests
 {
-    // TODO RH: Fix these
-    // [Fact]
-    // public void WhenNotCallingConfigure_ShouldNotResolveLimitedConcurrencyScheduler()
-    // {
-    //     var builder = new SiloBuilder();
-    //     
-    //     builder.UseLocalhostClustering();
-    //
-    //     var host = builder.Build();
-    //     var scheduler = (LimitedConcurrencyLevelTaskScheduler)host.Services.GetService(typeof(LimitedConcurrencyLevelTaskScheduler));
-    //
-    //     scheduler.Should().BeNull("The extension method was not used to register the scheduler");
-    // }
-    //
-    // [Theory]
-    // [InlineData(4)]
-    // [InlineData(8)]
-    // public void WhenCallingConfigure_ShouldRegisterLimitedConcurrencyScheduler(int maxSyncWorkConcurrency)
-    // {
-    //     var builder = new SiloBuilder();
-    //     builder.ConfigureSyncWorkAbstraction(maxSyncWorkConcurrency);
-    //
-    //     builder.UseLocalhostClustering();
-    //
-    //     var host = builder.Build();
-    //     var scheduler = (LimitedConcurrencyLevelTaskScheduler)host.Services.GetService(typeof(LimitedConcurrencyLevelTaskScheduler));
-    //
-    //     scheduler.Should().NotBeNull("the extension method was to registered the scheduler");
-    //     scheduler?.MaximumConcurrencyLevel.Should().Be(maxSyncWorkConcurrency,
-    //         "the scheduler should have the registered level of maximum concurrency");
-    // }
+    [Fact]
+    public void WhenNotCallingConfigure_ShouldNotResolveLimitedConcurrencyScheduler()
+    {
+        var builder = new HostBuilder()
+            .UseOrleans(orleansBuilder =>
+            {
+                orleansBuilder.UseLocalhostClustering();
+            });
+
+        var host = builder.Build();
+        var scheduler = (LimitedConcurrencyLevelTaskScheduler)host.Services.GetService(typeof(LimitedConcurrencyLevelTaskScheduler));
+
+        scheduler.Should().BeNull("The extension method was not used to register the scheduler");
+    }
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(8)]
+    public void WhenCallingConfigure_ShouldRegisterLimitedConcurrencyScheduler(int maxSyncWorkConcurrency)
+    {
+        var builder = new HostBuilder()
+            .UseOrleans(orleansBuilder =>
+            {
+                orleansBuilder.ConfigureSyncWorkAbstraction(maxSyncWorkConcurrency);
+                orleansBuilder.UseLocalhostClustering();
+            });
+
+        var host = builder.Build();
+        var scheduler = (LimitedConcurrencyLevelTaskScheduler)host.Services.GetService(typeof(LimitedConcurrencyLevelTaskScheduler));
+
+        scheduler.Should().NotBeNull("the extension method was to registered the scheduler");
+        scheduler?.MaximumConcurrencyLevel.Should().Be(maxSyncWorkConcurrency,
+            "the scheduler should have the registered level of maximum concurrency");
+    }
 }
