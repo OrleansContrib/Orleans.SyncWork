61b3751fd55fba863246f8f35dcb4371623c3ca8
Russ Hammett <kritner@gmail.com>
2024-10-21 07:50:02 -0400

    Introduces separate silo and client projects as a sample * Relates to #60

---
+++ samples/SeparateSiloAndClient/Silo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.SyncWork.Demo.Services;
+using Orleans.SyncWork.ExtensionMethods;
+
+Console.WriteLine("Hello, World!");
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering()
+            .ConfigureLogging(logging => logging.AddConsole());
+        silo.ConfigureSyncWorkAbstraction(2);
+    })
+    .UseConsoleLifetime()
+    .ConfigureServices(collection =>
+    {
+        collection.AddSingleton<IPasswordVerifier, PasswordVerifier>();
+    });
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
